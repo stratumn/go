1641ceccd764c00526c093a45026816ce49eacab
Adrien Montfort <adrien.montfort@gmail.com>
2017-11-30 11:39:20 +0100

    cmd: update license for private cmds

--- cmd/postgrestmpop/main.go
+++ cmd/postgrestmpop/main.go
@@ -1,8 +1,6 @@
-// Copyright 2017 Stratumn SAS. All rights reserved.
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright 2016 Stratumn SAS. All rights reserved.
+// Use of this source code is governed by the license that can be found in the
+// LICENSE file.
 
 // The command postgrestmpop starts a tmpop node with a postgresstore.
 package main
