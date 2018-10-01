c508e1995de9f5ba81c70c81008dd4f06cee7ea0
Bastien Teinturier <31281497+t-bast@users.noreply.github.com>
2018-10-01 14:51:34 +0200

    validations: validate out degree (#447)

--- validation/validators/refs.go
+++ validation/validators/refs.go
@@ -42,60 +42,94 @@ func NewRefsValidator() Validator {
 
 // Validate all references (parent and refs).
 func (v *RefsValidator) Validate(ctx context.Context, r store.SegmentReader, l *chainscript.Link) error {
-	if len(l.PrevLinkHash()) > 0 {
-		s, err := r.GetSegment(ctx, l.PrevLinkHash())
-		if err != nil {
-			return errors.WithStack(err)
-		}
-
-		if s == nil || s.Link == nil {
-			return ErrParentNotFound
-		}
-
-		if s.Link.Meta.Process.Name != l.Meta.Process.Name {
-			return ErrProcessMismatch
-		}
-
-		if s.Link.Meta.MapId != l.Meta.MapId {
-			return ErrMapIDMismatch
-		}
+	if err := v.validateParent(ctx, r, l); err != nil {
+		return err
 	}
 
-	if len(l.Meta.Refs) > 0 {
-		var lhs []chainscript.LinkHash
-		for _, ref := range l.Meta.Refs {
-			lhs = append(lhs, ref.LinkHash)
-		}
+	return v.validateReferences(ctx, r, l)
+}
 
-		segments, err := r.FindSegments(ctx, &store.SegmentFilter{
-			Pagination: store.Pagination{Limit: len(lhs)},
-			LinkHashes: lhs,
+func (v *RefsValidator) validateParent(ctx context.Context, r store.SegmentReader, l *chainscript.Link) error {
+	if len(l.PrevLinkHash()) == 0 {
+		return nil
+	}
+
+	parent, err := r.GetSegment(ctx, l.PrevLinkHash())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if parent == nil || parent.Link == nil {
+		return ErrParentNotFound
+	}
+
+	if parent.Link.Meta.Process.Name != l.Meta.Process.Name {
+		return ErrProcessMismatch
+	}
+
+	if parent.Link.Meta.MapId != l.Meta.MapId {
+		return ErrMapIDMismatch
+	}
+
+	if parent.Link.Meta.OutDegree == 0 {
+		return chainscript.ErrOutDegree
+	}
+
+	if parent.Link.Meta.OutDegree > 0 {
+		children, err := r.FindSegments(ctx, &store.SegmentFilter{
+			Pagination:   store.Pagination{Limit: 1},
+			PrevLinkHash: l.PrevLinkHash(),
 		})
 		if err != nil {
 			return errors.WithStack(err)
 		}
 
-		if len(segments.Segments) != len(l.Meta.Refs) {
-			return ErrRefNotFound
+		if int(parent.Link.Meta.OutDegree) <= children.TotalCount {
+			return chainscript.ErrOutDegree
+		}
+	}
+
+	return nil
+}
+
+func (v *RefsValidator) validateReferences(ctx context.Context, r store.SegmentReader, l *chainscript.Link) error {
+	if len(l.Meta.Refs) == 0 {
+		return nil
+	}
+
+	var lhs []chainscript.LinkHash
+	for _, ref := range l.Meta.Refs {
+		lhs = append(lhs, ref.LinkHash)
+	}
+
+	segments, err := r.FindSegments(ctx, &store.SegmentFilter{
+		Pagination: store.Pagination{Limit: len(lhs)},
+		LinkHashes: lhs,
+	})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if len(segments.Segments) != len(l.Meta.Refs) {
+		return ErrRefNotFound
+	}
+
+	for _, ref := range l.Meta.Refs {
+		found := false
+
+		for _, s := range segments.Segments {
+			if bytes.Equal(ref.LinkHash, s.LinkHash()) {
+				found = true
+				if ref.Process != s.Link.Meta.Process.Name {
+					return ErrProcessMismatch
+				}
+
+				break
+			}
 		}
 
-		for _, ref := range l.Meta.Refs {
-			found := false
-
-			for _, s := range segments.Segments {
-				if bytes.Equal(ref.LinkHash, s.LinkHash()) {
-					found = true
-					if ref.Process != s.Link.Meta.Process.Name {
-						return ErrProcessMismatch
-					}
-
-					break
-				}
-			}
-
-			if !found {
-				return ErrRefNotFound
-			}
+		if !found {
+			return ErrRefNotFound
 		}
 	}
 
