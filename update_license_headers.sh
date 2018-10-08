db7ab75ff1550b048424924d3abc9fe907f35d93
t-bast <bastien@stratumn.com>
2018-10-08 17:44:01 +0200

    license: PR comments

--- update_license_headers.sh
+++ update_license_headers.sh
@@ -6,7 +6,7 @@ update-license-header() {
     perl -i -0pe 's/\/\/ Copyright .* Stratumn.*\n(\/\/.*\n)*/`cat LICENSE_HEADER`/ge' $1
 }
 
-for f in $(find . -regex ".*\.go" | grep -v vendor); do
+for f in $(find . -name "*.go" | grep -v vendor); do
     echo Updating $f...
     update-license-header $f
 done
