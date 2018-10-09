34671f6dc4c9cb129a8bfe8bfa8b247763505bb6
Bastien Teinturier <31281497+t-bast@users.noreply.github.com>
2018-10-09 09:29:34 +0200

    *: update license header and pin go-chainscript (#453)

---
+++ update_license_headers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+update-license-header() {
+    perl -i -0pe 's/\/\/ Copyright .* Stratumn.*\n(\/\/.*\n)*/`cat LICENSE_HEADER`/ge' $1
+}
+
+for f in $(find . -name "*.go" | grep -v vendor); do
+    echo Updating $f...
+    update-license-header $f
+done
