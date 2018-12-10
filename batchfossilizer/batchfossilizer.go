22d86f977fefb0cd629fe60198bd7dc8c73bbb5c
t-bast <bastien@stratumn.com>
2018-12-10 10:27:30 +0100

    batchfossilizer: implement new batch fossilizer

--- batchfossilizer/batchfossilizer.go
+++ batchfossilizer/batchfossilizer.go
@@ -19,9 +19,16 @@ package batchfossilizer
 
 import (
 	"context"
+	"encoding/hex"
 	"sync"
+	"time"
 
+	"github.com/stratumn/go-core/batchfossilizer/evidences"
 	"github.com/stratumn/go-core/fossilizer"
+	"github.com/stratumn/go-core/monitoring"
+	"github.com/stratumn/merkle"
+
+	"go.opencensus.io/trace"
 )
 
 const (
@@ -40,22 +47,56 @@ type Info struct {
 	Commit      string `json:"commit"`
 }
 
+// PendingProof is used to store an incomplete proof (waiting for the proof of
+// the wrapped fossilizer).
+type PendingProof struct {
+	fossil *fossilizer.Fossil
+	proof  *evidences.BatchProof
+}
+
 // Fossilizer is the type that
 // implements github.com/stratumn/go-core/fossilizer.Adapter.
 type Fossilizer struct {
 	config *Config
+	ticker *time.Ticker
 
-	fossilizerEventMutex sync.Mutex
-	fossilizerEventChans []chan *fossilizer.Event
+	foss  fossilizer.Adapter
+	queue fossilizer.FossilsQueue
+
+	eventChansLock sync.RWMutex
+	eventChans     []chan *fossilizer.Event
+
+	pendingProofsLock sync.Mutex
+	pendingProofs     map[string][]*PendingProof
 }
 
-// New creates an instance of a Fossilizer.
-func New(config *Config) fossilizer.Adapter {
-	a := &Fossilizer{config: config}
+// New creates an instance of a batch Fossilizer by wrapping an existing
+// fossilizer.
+// You should cancel the input context to properly free internal go routines
+// when you don't need the fossilizer.
+func New(ctx context.Context, config *Config, f fossilizer.Adapter, q fossilizer.FossilsQueue) fossilizer.Adapter {
+	t := time.NewTicker(config.GetInterval())
+	a := &Fossilizer{
+		config:        config,
+		ticker:        t,
+		foss:          f,
+		queue:         q,
+		pendingProofs: make(map[string][]*PendingProof),
+	}
+
+	// The wrapped fossilizer will be the one doing the actual fossilization
+	// and producing a blockchain proof.
+	// So we need to listen to its events and re-trigger our own events on top.
+	fChan := make(chan *fossilizer.Event)
+	f.AddFossilizerEventChan(fChan)
+	go a.eventLoop(ctx, fChan)
+
+	go a.fossilizeLoop(ctx)
+
 	return a
 }
 
-// GetInfo implements github.com/stratumn/go-core/fossilizer.Adapter.GetInfo.
+// GetInfo returns the fossilizer info.
 func (a *Fossilizer) GetInfo(ctx context.Context) (interface{}, error) {
 	return &Info{
 		Name:        Name,
@@ -65,19 +106,163 @@ func (a *Fossilizer) GetInfo(ctx context.Context) (interface{}, error) {
 	}, nil
 }
 
-// AddFossilizerEventChan implements
-// github.com/stratumn/go-core/fossilizer.Adapter.AddFossilizerEventChan.
+// AddFossilizerEventChan forwards to the underlying fossilizer.
 func (a *Fossilizer) AddFossilizerEventChan(fossilizerEventChan chan *fossilizer.Event) {
-	a.fossilizerEventMutex.Lock()
-	defer a.fossilizerEventMutex.Unlock()
+	a.eventChansLock.Lock()
+	defer a.eventChansLock.Unlock()
 
-	a.fossilizerEventChans = append(a.fossilizerEventChans, fossilizerEventChan)
+	a.eventChans = append(a.eventChans, fossilizerEventChan)
 }
 
-// Fossilize implements github.com/stratumn/go-core/fossilizer.Adapter.Fossilize.
+// Fossilize adds to the fossilizing queue.
 func (a *Fossilizer) Fossilize(ctx context.Context, data []byte, meta []byte) error {
-	// TODO: just add to the queue.
-	// TODO: go routine to dequeue every config.GetInterval() and actually
-	// fossilize with the wrapped fossilizer.
-	return nil
+	return a.queue.Push(ctx, &fossilizer.Fossil{
+		Data: data,
+		Meta: meta,
+	})
+}
+
+func (a *Fossilizer) fossilizeLoop(ctx context.Context) {
+	for {
+		select {
+		case <-a.ticker.C:
+			a.fossilizeBatch(context.Background())
+		case <-ctx.Done():
+			a.ticker.Stop()
+			return
+		}
+	}
+}
+
+func (a *Fossilizer) fossilizeBatch(ctx context.Context) {
+	ctx, span := trace.StartSpan(ctx, "batchfossilizer/fossilizeBatch")
+	defer span.End()
+
+	batchSize := a.config.GetMaxLeaves()
+
+	for i := 0; i < a.config.GetMaxSimBatches(); i++ {
+		fossils, err := a.queue.Pop(ctx, batchSize)
+		if err != nil {
+			monitoring.SetSpanStatus(span, err)
+			return
+		}
+
+		if len(fossils) == 0 {
+			return
+		}
+
+		leaves := make([][]byte, len(fossils))
+		for i := 0; i < len(fossils); i++ {
+			leaves[i] = fossils[i].Data
+		}
+
+		tree, err := merkle.NewStaticTree(leaves)
+		if err != nil {
+			monitoring.SetSpanStatus(span, err)
+			return
+		}
+
+		root := tree.Root()
+		a.addPendingProofs(fossils, root, tree)
+
+		err = a.foss.Fossilize(ctx, root, nil)
+		if err != nil {
+			monitoring.SetSpanStatus(span, err)
+			return
+		}
+
+		// If the queue is empty, early return instead of polling the queue.
+		if len(fossils) < batchSize {
+			return
+		}
+	}
+}
+
+// addPendingProofs pre-fills proofs for all the fossils that are pending.
+// Once the wrapped fossilizer confirms fossilization of the merkle tree, we
+// can create fossilization events for all those pending proofs.
+func (a *Fossilizer) addPendingProofs(fossils []*fossilizer.Fossil, root []byte, tree *merkle.StaticTree) {
+	a.pendingProofsLock.Lock()
+	defer a.pendingProofsLock.Unlock()
+
+	pendingProofs := make([]*PendingProof, len(fossils))
+	for i := 0; i < len(fossils); i++ {
+		pendingProofs[i] = &PendingProof{
+			fossil: fossils[i],
+			proof: &evidences.BatchProof{
+				Root: root,
+				Path: tree.Path(i),
+			},
+		}
+	}
+
+	key := hex.EncodeToString(root)
+	a.pendingProofs[key] = pendingProofs
+}
+
+func (a *Fossilizer) eventLoop(ctx context.Context, fChan chan *fossilizer.Event) {
+	for {
+		select {
+		case e := <-fChan:
+			a.eventBatch(context.Background(), e)
+		case <-ctx.Done():
+			close(fChan)
+			return
+		}
+	}
+}
+
+// eventBatch transforms a single fossilization event of a merkle root into
+// individual fossilization events for each fossil included in the merkle tree.
+// It then sends these events to all registered listeners.
+func (a *Fossilizer) eventBatch(ctx context.Context, e *fossilizer.Event) {
+	ctx, span := trace.StartSpan(ctx, "batchfossilizer/eventBatch")
+	defer span.End()
+
+	if e.EventType != fossilizer.DidFossilize {
+		return
+	}
+
+	r := e.Data.(*fossilizer.Result)
+	key := hex.EncodeToString(r.Data)
+
+	a.pendingProofsLock.Lock()
+	defer a.pendingProofsLock.Unlock()
+
+	pendingProofs, ok := a.pendingProofs[key]
+	if !ok {
+		span.Annotatef(nil, "pending proofs not found for root %s", key)
+		return
+	}
+
+	a.eventChansLock.RLock()
+	defer a.eventChansLock.RUnlock()
+
+	for _, p := range pendingProofs {
+		// Wrap the underlying (blockchain) proof.
+		p.proof.Timestamp = time.Now().Unix()
+		p.proof.Proof = r.Evidence.Proof
+		ev, err := p.proof.Evidence(Name)
+		if err != nil {
+			span.Annotate(
+				[]trace.Attribute{trace.StringAttribute("error", err.Error())},
+				"could not create evidence",
+			)
+			continue
+		}
+
+		for _, l := range a.eventChans {
+			go func(l chan *fossilizer.Event) {
+				l <- &fossilizer.Event{
+					EventType: fossilizer.DidFossilize,
+					Data: &fossilizer.Result{
+						Fossil:   *p.fossil,
+						Evidence: *ev,
+					},
+				}
+			}(l)
+		}
+	}
+
+	delete(a.pendingProofs, key)
 }
--- batchfossilizer/batchfossilizer_test.go
+++ batchfossilizer/batchfossilizer_test.go
@@ -13,3 +13,233 @@
 // limitations under the License.
 
 package batchfossilizer_test
+
+import (
+	"context"
+	"encoding/hex"
+	"testing"
+	"time"
+
+	json "github.com/gibson042/canonicaljson-go"
+	"github.com/stratumn/go-chainscript"
+	"github.com/stratumn/go-core/batchfossilizer"
+	"github.com/stratumn/go-core/batchfossilizer/evidences"
+	"github.com/stratumn/go-core/dummyfossilizer"
+	dummyevidences "github.com/stratumn/go-core/dummyfossilizer/evidences"
+	"github.com/stratumn/go-core/fossilizer"
+	"github.com/stratumn/go-core/fossilizer/dummyqueue"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func batchProof(t *testing.T, e *chainscript.Evidence) *evidences.BatchProof {
+	var proof evidences.BatchProof
+	err := json.Unmarshal(e.Proof, &proof)
+	require.NoError(t, err)
+
+	return &proof
+}
+
+func TestGetInfo(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	f := batchfossilizer.New(
+		ctx,
+		&batchfossilizer.Config{
+			Version: "1.0.0",
+			Commit:  "abcdef",
+		},
+		dummyfossilizer.New(&dummyfossilizer.Config{}),
+		dummyqueue.New(),
+	)
+
+	i, err := f.GetInfo(ctx)
+	require.NoError(t, err)
+	assert.Equal(t, "1.0.0", i.(*batchfossilizer.Info).Version)
+	assert.Equal(t, "abcdef", i.(*batchfossilizer.Info).Commit)
+}
+
+func TestFossilize(t *testing.T) {
+	t.Run("nothing to fossilize", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		f := dummyfossilizer.New(&dummyfossilizer.Config{})
+		q := dummyqueue.New()
+		batch := batchfossilizer.New(
+			ctx,
+			&batchfossilizer.Config{Interval: 3 * time.Millisecond},
+			f,
+			q,
+		)
+
+		eventChan := make(chan *fossilizer.Event, 1)
+		batch.AddFossilizerEventChan(eventChan)
+
+		select {
+		case <-eventChan:
+			assert.Fail(t, "unexpected fossilizer event")
+		case <-time.After(5 * time.Millisecond):
+		}
+	})
+
+	t.Run("single element", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		f := dummyfossilizer.New(&dummyfossilizer.Config{})
+		q := dummyqueue.New()
+		batch := batchfossilizer.New(
+			ctx,
+			&batchfossilizer.Config{Interval: 3 * time.Millisecond},
+			f,
+			q,
+		)
+
+		eventChan := make(chan *fossilizer.Event)
+		batch.AddFossilizerEventChan(eventChan)
+
+		err := batch.Fossilize(ctx, []byte("b4tm4n"), []byte("r0b1n"))
+		require.NoError(t, err)
+
+		e := <-eventChan
+		assert.Equal(t, fossilizer.DidFossilize, e.EventType)
+
+		r, ok := e.Data.(*fossilizer.Result)
+		require.True(t, ok)
+		assert.Equal(t, []byte("b4tm4n"), r.Data)
+		assert.Equal(t, []byte("r0b1n"), r.Meta)
+
+		// The queue should have been emptied.
+		assert.Empty(t, q.Fossils())
+	})
+
+	t.Run("max leaves", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		f := dummyfossilizer.New(&dummyfossilizer.Config{})
+		q := dummyqueue.New()
+		batch := batchfossilizer.New(
+			ctx,
+			&batchfossilizer.Config{
+				Interval:  3 * time.Millisecond,
+				MaxLeaves: 2,
+			},
+			f,
+			q,
+		)
+
+		eventChan := make(chan *fossilizer.Event, 3)
+		batch.AddFossilizerEventChan(eventChan)
+
+		for i := byte(0); i < 3; i++ {
+			err := batch.Fossilize(ctx, []byte{i}, []byte{i + 10})
+			require.NoError(t, err)
+		}
+
+		trees := make(map[string]struct{})
+		for i := byte(0); i < 3; i++ {
+			e := <-eventChan
+			r, ok := e.Data.(*fossilizer.Result)
+			require.True(t, ok)
+
+			p := batchProof(t, &r.Evidence)
+			trees[hex.EncodeToString(p.Root)] = struct{}{}
+		}
+
+		// The fossils should have been split into two merkle trees.
+		assert.Len(t, trees, 2)
+		assert.Empty(t, q.Fossils())
+	})
+
+	t.Run("simultaneous batches", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		f := dummyfossilizer.New(&dummyfossilizer.Config{})
+		q := dummyqueue.New()
+		batch := batchfossilizer.New(
+			ctx,
+			&batchfossilizer.Config{
+				Interval:      3 * time.Millisecond,
+				MaxLeaves:     2,
+				MaxSimBatches: 2,
+			},
+			f,
+			q,
+		)
+
+		eventChan := make(chan *fossilizer.Event, 5)
+		batch.AddFossilizerEventChan(eventChan)
+
+		for i := byte(0); i < 5; i++ {
+			err := batch.Fossilize(ctx, []byte{i}, []byte{i + 10})
+			require.NoError(t, err)
+		}
+
+		trees := make(map[string]struct{})
+		for i := byte(0); i < 5; i++ {
+			e := <-eventChan
+			r, ok := e.Data.(*fossilizer.Result)
+			require.True(t, ok)
+
+			p := batchProof(t, &r.Evidence)
+			trees[hex.EncodeToString(p.Root)] = struct{}{}
+		}
+
+		// The fossils should have been split into three merkle trees.
+		assert.Len(t, trees, 3)
+		assert.Empty(t, q.Fossils())
+	})
+
+	t.Run("fossilizer evidence", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		f := dummyfossilizer.New(&dummyfossilizer.Config{})
+		q := dummyqueue.New()
+		batch := batchfossilizer.New(
+			ctx,
+			&batchfossilizer.Config{
+				Interval:  3 * time.Millisecond,
+				MaxLeaves: 10,
+			},
+			f,
+			q,
+		)
+
+		eventChan := make(chan *fossilizer.Event, 3)
+		batch.AddFossilizerEventChan(eventChan)
+
+		for i := byte(0); i < 3; i++ {
+			err := batch.Fossilize(ctx, []byte{i}, []byte{i + 10})
+			require.NoError(t, err)
+		}
+
+		now := time.Now().Unix()
+		var root []byte
+		for i := 0; i < 3; i++ {
+			e := <-eventChan
+			r, ok := e.Data.(*fossilizer.Result)
+			require.True(t, ok)
+
+			p := batchProof(t, &r.Evidence)
+			if root == nil {
+				root = p.Root
+			}
+
+			// All fossils should be in the same merkle tree.
+			assert.Equal(t, root, p.Root)
+			assert.InDelta(t, now, p.Timestamp, 10)
+			assert.NoError(t, p.Path.Validate())
+
+			// We should find an evidence produced by the wrapped fossilizer.
+			var proof dummyevidences.DummyProof
+			err := json.Unmarshal(p.Proof, &proof)
+			require.NoError(t, err)
+			assert.InDelta(t, now, proof.Timestamp, 10)
+		}
+	})
+}
