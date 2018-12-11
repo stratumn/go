ffd821f468f83838c79c281c582008eedcb819e1
t-bast <bastien@stratumn.com>
2018-12-11 09:55:40 +0100

    aws: create empty queue implementation

---
+++ cloud/aws/queue.go
@@ -0,0 +1,39 @@
+// Copyright 2016-2018 Stratumn SAS. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"context"
+
+	"github.com/stratumn/go-core/fossilizer"
+)
+
+// FossilsQueue implements a fossils queue backed by AWS SQS.
+type FossilsQueue struct{}
+
+// NewFossilsQueue blablabla
+func NewFossilsQueue() fossilizer.FossilsQueue {
+	return &FossilsQueue{}
+}
+
+// Push a fossil to the queue.
+func (q *FossilsQueue) Push(context.Context, *fossilizer.Fossil) error {
+	return nil
+}
+
+// Pop fossils from the queue.
+func (q *FossilsQueue) Pop(context.Context, int) ([]*fossilizer.Fossil, error) {
+	return nil, nil
+}
